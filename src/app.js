a62132bd82c178d0187af1bf95f967208b97167b
Felipe <felari94@gmail.com>
2024-11-06 12:04:35 -0300

    wip - documentacion

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
-import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
 import router from "./routes/index.js";
+
+import swaggerUiExpress from "swagger-ui-express";
+import { specs } from "./utils/config/swagger.config.js";
 import { errorHandle } from "./errors/errorHandler.js";
 import { connectDB } from "./utils/config/dbconnection.js";
 
@@ -13,6 +15,7 @@ connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api-docs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 app.use("/api", router);
 
